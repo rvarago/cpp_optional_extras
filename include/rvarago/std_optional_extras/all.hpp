085576989337ea0769292cf6412ff499b820926b
Rafael Varago <rafael.varago@gmail.com>
2025-01-22 17:23:33 +0100

    mark functions as noexcept
    Since they shall not throw.

--- include/rvarago/std_optional_extras/all.hpp
+++ include/rvarago/std_optional_extras/all.hpp
@@ -6,7 +6,8 @@ namespace rvarago::std_optional_extras {
 // `filter(opt, pred)` is `opt` when `opt` is engaged and its value
 // satisfies `pred`, otherwise is none.
 template <typename T>
-constexpr auto filter(std::optional<T> opt, std::predicate<T> auto pred)
+constexpr auto filter(std::optional<T> opt,
+                      std::predicate<T> auto pred) noexcept
     -> std::optional<T> {
   if (opt && pred(*opt)) {
     return opt;
@@ -18,7 +19,8 @@ constexpr auto filter(std::optional<T> opt, std::predicate<T> auto pred)
 // `contains(opt, value)` is `true` when `opt` is engaged and its value
 // equals `value`, otherwise is `false`.
 template <typename T>
-constexpr auto contains(std::optional<T> const &opt, T const &value) -> bool {
+constexpr auto contains(std::optional<T> const &opt, T const &value) noexcept
+    -> bool {
   return filter(opt, [&value](auto const &v) { return v == value; })
       .has_value();
 }

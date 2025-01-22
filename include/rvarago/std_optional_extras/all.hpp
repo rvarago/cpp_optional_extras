b6c8d691885ef59522dd94b672119da3490c4ad6
Rafael Varago <rafael.varago@gmail.com>
2025-01-22 17:03:11 +0100

    add filter(opt<T>, T -> bool) -> opt<T>

--- include/rvarago/std_optional_extras/all.hpp
+++ include/rvarago/std_optional_extras/all.hpp
@@ -1,5 +1,18 @@
+#include <concepts>
+#include <optional>
+
 namespace rvarago::std_optional_extras {
 
-template <typename T> constexpr auto id(T v) -> T { return v; }
+// `filter(opt, pred)` is `opt` when `opt` is engaged and its value
+// satisfies `pred`, otherwise is none.
+template <typename T>
+constexpr auto filter(std::optional<T> opt, std::predicate<T> auto pred)
+    -> std::optional<T> {
+  if (opt && pred(*opt)) {
+    return opt;
+  } else {
+    return std::nullopt;
+  }
+}
 
 } // namespace rvarago::std_optional_extras
--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,17 @@
 
 #include <rvarago/std_optional_extras/all.hpp>
 
+#include <optional>
+
 namespace optx = rvarago::std_optional_extras;
 
-TEST_CASE("id is identity", "[id]") { static_assert(optx::id(42) == 42); }
+template <typename T> constexpr std::optional<T> none;
+
+TEST_CASE("Filter-in when engaged with a value satisfying a predicate",
+          "[filter]") {
+  constexpr auto is_even = [](int const x) -> bool { return (x % 2) == 0; };
+
+  STATIC_REQUIRE(optx::filter(none<int>, is_even) == none<int>);
+  STATIC_REQUIRE(optx::filter(std::optional{1}, is_even) == none<int>);
+  STATIC_REQUIRE(optx::filter(std::optional{42}, is_even) == std::optional{42});
+}

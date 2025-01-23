01e24eb30bfd79f05b4bef04ff2c540d9d8959ff
Rafael Varago <rafael.varago@gmail.com>
2025-01-23 15:32:56 +0100

    use std::optional unqualified in tests
    To shorten up syntax a little and there's no real
    ambiguity.

--- test/main.cpp
+++ test/main.cpp
@@ -7,7 +7,9 @@
 
 namespace optx = rvarago::optional_extras;
 
-template <typename T> constexpr std::optional<T> none;
+using std::optional;
+
+template <typename T> constexpr optional<T> none;
 
 TEST_CASE("When engaged with a value satisfying a predicate", "[filter]") {
   static constexpr auto is_even = [](int const x) -> bool {
@@ -15,14 +17,14 @@ TEST_CASE("When engaged with a value satisfying a predicate", "[filter]") {
   };
 
   STATIC_REQUIRE(optx::filter(none<int>, is_even) == none<int>);
-  STATIC_REQUIRE(optx::filter(std::optional{1}, is_even) == none<int>);
-  STATIC_REQUIRE(optx::filter(std::optional{42}, is_even) == std::optional{42});
+  STATIC_REQUIRE(optx::filter(optional{1}, is_even) == none<int>);
+  STATIC_REQUIRE(optx::filter(optional{42}, is_even) == optional{42});
 }
 
 TEST_CASE("When engaged with a value equal to the argument", "[contains]") {
   STATIC_REQUIRE(optx::contains(none<int>, 42) == false);
-  STATIC_REQUIRE(optx::contains(std::optional{1}, 42) == false);
-  STATIC_REQUIRE(optx::contains(std::optional{42}, 42) == true);
+  STATIC_REQUIRE(optx::contains(optional{1}, 42) == false);
+  STATIC_REQUIRE(optx::contains(optional{42}, 42) == true);
 }
 
 TEST_CASE("When both are engaged then merge them", "[zip_with]") {
@@ -31,21 +33,20 @@ TEST_CASE("When both are engaged then merge them", "[zip_with]") {
   };
 
   STATIC_REQUIRE(optx::zip_with(none<bool>, none<int>, merge) == none<char>);
-  STATIC_REQUIRE(optx::zip_with(std::optional{false}, none<int>, merge) ==
+  STATIC_REQUIRE(optx::zip_with(optional{false}, none<int>, merge) ==
                  none<char>);
-  STATIC_REQUIRE(optx::zip_with(none<bool>, std::optional{42}, merge) ==
-                 none<char>);
-  STATIC_REQUIRE(optx::zip_with(std::optional{false}, std::optional{42},
-                                merge) == std::optional{'A'});
+  STATIC_REQUIRE(optx::zip_with(none<bool>, optional{42}, merge) == none<char>);
+  STATIC_REQUIRE(optx::zip_with(optional{false}, optional{42}, merge) ==
+                 optional{'A'});
 }
 
 TEST_CASE("When both are engaged then pair them", "[zip]") {
   STATIC_REQUIRE(optx::zip(none<bool>, none<int>) ==
                  none<std::pair<bool, int>>);
-  STATIC_REQUIRE(optx::zip(std::optional{false}, none<int>) ==
+  STATIC_REQUIRE(optx::zip(optional{false}, none<int>) ==
                  none<std::pair<bool, int>>);
-  STATIC_REQUIRE(optx::zip(none<bool>, std::optional{42}) ==
+  STATIC_REQUIRE(optx::zip(none<bool>, optional{42}) ==
                  none<std::pair<bool, int>>);
-  STATIC_REQUIRE(optx::zip(std::optional{false}, std::optional{42}) ==
-                 std::optional{std::pair{false, 42}});
+  STATIC_REQUIRE(optx::zip(optional{false}, optional{42}) ==
+                 optional{std::pair{false, 42}});
 }

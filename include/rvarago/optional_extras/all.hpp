90c0d41c4ed64e91b5bb6d3c232966b6c6f5b3d3
Rafael Varago <rafael.varago@gmail.com>
2025-01-23 15:00:18 +0100

    require fewer operations on std::optional
    Essentially, default-construct it instead of `std::nullopt` and
    cast to a boolean instead of calling into `has_value`. Although
    the outcome is more verbose, this paves the way for programming
    against an `optional_like` concept with fewer requirements.

--- include/rvarago/optional_extras/all.hpp
+++ include/rvarago/optional_extras/all.hpp
@@ -6,23 +6,23 @@ namespace rvarago::optional_extras {
 // `filter(opt, pred)` is `opt` when `opt` is engaged and its value
 // satisfies `pred`, otherwise is none.
 template <typename T>
-constexpr auto filter(std::optional<T> opt,
-                      std::predicate<T> auto pred) noexcept
-    -> std::optional<T> {
+constexpr auto
+filter(std::optional<T> opt,
+       std::predicate<T> auto pred) noexcept -> std::optional<T> {
   if (opt && pred(*opt)) {
     return opt;
   } else {
-    return std::nullopt;
+    return std::optional<T>{};
   }
 }
 
 // `contains(opt, value)` is `true` when `opt` is engaged and its value
 // equals `value`, otherwise is `false`.
 template <typename T>
-constexpr auto contains(std::optional<T> const &opt, T const &value) noexcept
-    -> bool {
-  return filter(opt, [&value](auto const &v) { return v == value; })
-      .has_value();
+constexpr auto contains(std::optional<T> const &opt,
+                        T const &value) noexcept -> bool {
+  return static_cast<bool>(
+      filter(opt, [&value](auto const &v) { return v == value; }));
 }
 
 } // namespace rvarago::optional_extras
